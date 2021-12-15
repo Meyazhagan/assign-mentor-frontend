19547bf253a5662d53e591e8501a19b641352470
Meyazhagan <meyazhagan.ofcl@gmail.com>
2021-12-15 10:35:12 +0530

    fixed a bug - multiple get request

--- src/components/app/StudentPage.js
+++ src/components/app/StudentPage.js
@@ -7,91 +7,93 @@ import { StudentContext } from "../../context/StudentContext";
 import MentorToStudent from "../common/MentorToStudent";
 
 function StudentPage() {
-  const { current, get, assign, unassign } = useContext(StudentContext);
-  const { get: getBatch } = useContext(BatchContext);
-  const { get: getMentor, mentors } = useContext(MentorContext);
+    const { current, get, assign, unassign } = useContext(StudentContext);
+    const { get: getBatch } = useContext(BatchContext);
+    const { get: getMentor, mentors } = useContext(MentorContext);
 
-  const [assignToggler, setAssignToggler] = useState(false);
+    const [assignToggler, setAssignToggler] = useState(false);
 
-  const navigate = useNavigate();
-  const params = useParams();
-  const batch_id = params.batchId;
-  const student_id = params.studentId;
+    const navigate = useNavigate();
+    const params = useParams();
+    const batch_id = params.batchId;
+    const student_id = params.studentId;
 
-  // const getStudent = async () => {
-  //   const student = await get(student_id);
-  //   console.log(student);
-  //   setStudent(student);
-  // };
-  useEffect(() => {
-    get(student_id);
-  }, [student_id, get]);
+    // const getStudent = async () => {
+    //   const student = await get(student_id);
+    //   console.log(student);
+    //   setStudent(student);
+    // };
+    useEffect(
+        () => {
+            get(student_id);
+        },
+        // eslint-disable-next-line
+        [student_id]
+    );
 
-  const handleAssign = (mentor_id) => {
-    assign(student_id, mentor_id);
-  };
-  const handleUnassign = () => {
-    unassign(student_id);
-  };
-  return (
-    <div>
-      <div className="flex justify-between items-center flex-wrap">
-        <div className="flex items-center gap-3">
-          <button
-            onClick={() => navigate("../")}
-            className={classNames(
-              `font-bold text-red-600
-            hover:bg-gray-200 py-2 px-4
-            rounded-md`
-            )}
-          >
-            Back
-          </button>
-          <div>Student Name - {current?.name}</div>
-        </div>
+    const handleAssign = (mentor_id) => {
+        assign(student_id, mentor_id);
+    };
+    const handleUnassign = () => {
+        unassign(student_id);
+    };
+    return (
         <div>
-          <button
-            onClick={() => setAssignToggler((prev) => !prev)}
-            className={classNames(
-              `font-bold text-green-600
+            <div className="flex justify-between items-center flex-wrap">
+                <div className="flex items-center gap-3">
+                    <button
+                        onClick={() => navigate("../")}
+                        className={classNames(
+                            `font-bold text-red-600
             hover:bg-gray-200 py-2 px-4
             rounded-md`
-            )}
-          >
-            Assign Mentor
-          </button>
+                        )}>
+                        Back
+                    </button>
+                    <div>Student Name - {current?.name}</div>
+                </div>
+                <div>
+                    <button
+                        onClick={() => setAssignToggler((prev) => !prev)}
+                        className={classNames(
+                            `font-bold text-green-600
+            hover:bg-gray-200 py-2 px-4
+            rounded-md`
+                        )}>
+                        Assign Mentor
+                    </button>
+                </div>
+            </div>
+            <div className="">
+                <div className="overflow-auto whitespace-nowrap">
+                    {current?.mentor ? (
+                        <MentorToStudent
+                            mentors={[getMentor(current?.mentor)]}
+                            actionLable={"Unassign"}
+                            onSubmit={handleUnassign}
+                            title={`Mentor Assigned to Student ${current?.name}`}
+                        />
+                    ) : (
+                        <div className="mt-10">There is No Mentor assigned. </div>
+                    )}
+                </div>
+                <div className="overflow-auto whitespace-nowrap">
+                    {assignToggler &&
+                        (mentors.length > 0 ? (
+                            <MentorToStudent
+                                skip={current.mentor}
+                                mentors={mentors}
+                                actionLable={"Assign"}
+                                onSubmit={handleAssign}
+                                title={`All Mentors in Batch - ${getBatch(batch_id)?.name}`}
+                            />
+                        ) : (
+                            <div className="mt-10">There is no Mentor.</div>
+                        ))}
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="">
-        <div className="overflow-auto whitespace-nowrap">
-          {current?.mentor ? (
-            <MentorToStudent
-              mentors={[getMentor(current?.mentor)]}
-              actionLable={"Unassign"}
-              onSubmit={handleUnassign}
-              title={`Mentor Assigned to Student ${current?.name}`}
-            />
-          ) : (
-            <div className="mt-10">There is No Mentor assigned. </div>
-          )}
-        </div>
-        <div className="overflow-auto whitespace-nowrap">
-          {assignToggler &&
-            (mentors.length > 0 ? (
-              <MentorToStudent
-                skip={current.mentor}
-                mentors={mentors}
-                actionLable={"Assign"}
-                onSubmit={handleAssign}
-                title={`All Mentors in Batch - ${getBatch(batch_id)?.name}`}
-              />
-            ) : (
-              <div className="mt-10">There is no Mentor.</div>
-            ))}
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default StudentPage;
--- src/context/MentorContext.js
+++ src/context/MentorContext.js
@@ -9,170 +9,170 @@ import { Toastify } from "../services/ToastServices";
 export const MentorContext = createContext();
 
 function MentorProvider({ children }) {
-  const [mentors, setMentors] = useState([]);
-  const [assignedStudents, setAssignedStudents] = useState([]);
-  const [unassignedStudents, setUnAssignedStudents] = useState([]);
+    const [mentors, setMentors] = useState([]);
+    const [assignedStudents, setAssignedStudents] = useState([]);
+    const [unassignedStudents, setUnAssignedStudents] = useState([]);
 
-  const getIndex = (id) => mentors.findIndex((b) => b._id === id);
+    const getIndex = (id) => mentors.findIndex((b) => b._id === id);
 
-  const fetchAll = async () => {
-    try {
-      const { data: mentors } = await mentorServices.getALl();
-      setMentors(mentors);
-    } catch (ex) {}
-  };
+    const fetchAll = async () => {
+        try {
+            const { data: mentors } = await mentorServices.getALl();
+            setMentors(mentors);
+        } catch (ex) {}
+    };
 
-  const get = (id) => {
-    const index = getIndex(id);
-    return mentors[index];
-  };
+    const get = (id) => {
+        const index = getIndex(id);
+        return mentors[index];
+    };
 
-  const create = async (newMentor) => {
-    const prevMentor = mentors;
+    const create = async (newMentor) => {
+        const prevMentor = mentors;
 
-    Toastify(mentorServices.create(newMentor), {
-      pending: "Creating the New Mentor",
+        Toastify(mentorServices.create(newMentor), {
+            pending: "Creating the New Mentor",
 
-      onSuccess: ({ data }) => {
-        setMentors([...mentors, data]);
-        return "Created The New Mentor";
-      },
+            onSuccess: ({ data }) => {
+                setMentors([...mentors, data]);
+                return "Created The New Mentor";
+            },
 
-      onError: (data) => {
-        setMentors(prevMentor);
-        return data?.response?.data?.message || "An Unexpected Error";
-      },
-    });
-  };
+            onError: (data) => {
+                setMentors(prevMentor);
+                return data?.response?.data?.message || "An Unexpected Error";
+            },
+        });
+    };
 
-  const update = async (id, updatedMentor) => {
-    const prevMentors = mentors;
+    const update = async (id, updatedMentor) => {
+        const prevMentors = mentors;
 
-    const index = getIndex(id);
-    const newMentors = [...mentors];
-    newMentors[index] = { ...newMentors[index], ...updatedMentor };
+        const index = getIndex(id);
+        const newMentors = [...mentors];
+        newMentors[index] = { ...newMentors[index], ...updatedMentor };
 
-    setMentors(newMentors);
+        setMentors(newMentors);
 
-    Toastify(mentorServices.update(id, updatedMentor), {
-      pending: "Updating the Mentor",
+        Toastify(mentorServices.update(id, updatedMentor), {
+            pending: "Updating the Mentor",
 
-      onSuccess: () => {
-        return "Updated the Mentor";
-      },
+            onSuccess: () => {
+                return "Updated the Mentor";
+            },
 
-      onError: (data) => {
-        setMentors(prevMentors);
-        return data?.response?.data?.message || "An Unexpected Error";
-      },
-    });
-  };
+            onError: (data) => {
+                setMentors(prevMentors);
+                return data?.response?.data?.message || "An Unexpected Error";
+            },
+        });
+    };
 
-  const remove = async (id) => {
-    const prevMentors = mentors;
+    const remove = async (id) => {
+        const prevMentors = mentors;
 
-    const index = getIndex(id);
-    const newMentors = [...mentors];
-    newMentors.splice(index, 1);
+        const index = getIndex(id);
+        const newMentors = [...mentors];
+        newMentors.splice(index, 1);
 
-    setMentors(newMentors);
+        setMentors(newMentors);
 
-    // toast message - promise
-    Toastify(mentorServices.remove(id), {
-      pending: "Deleting the Mentor",
+        // toast message - promise
+        Toastify(mentorServices.remove(id), {
+            pending: "Deleting the Mentor",
 
-      onSuccess: () => {
-        return "Deleted the Mentor";
-      },
+            onSuccess: () => {
+                return "Deleted the Mentor";
+            },
 
-      onError: (data) => {
-        setMentors(prevMentors);
-        return data?.response?.data?.message || "An Unexpected Error";
-      },
-    });
-  };
+            onError: (data) => {
+                setMentors(prevMentors);
+                return data?.response?.data?.message || "An Unexpected Error";
+            },
+        });
+    };
 
-  const getAssignedStudents = async (mentorId) => {
-    try {
-      const { data: assigned } = await assignServices.getAll(mentorId);
-      setAssignedStudents(assigned);
-    } catch (ex) {}
-  };
+    const getAssignedStudents = async (mentorId) => {
+        try {
+            const { data: assigned } = await assignServices.getAll(mentorId);
+            setAssignedStudents(assigned);
+        } catch (ex) {}
+    };
 
-  const getUnassignedStudents = async () => {
-    try {
-      const { data: unassigned } = await unassignServices.getAll();
-      setUnAssignedStudents(unassigned);
-    } catch (ex) {}
-  };
+    const getUnassignedStudents = async () => {
+        try {
+            const { data: unassigned } = await unassignServices.getAll();
+            setUnAssignedStudents(unassigned);
+        } catch (ex) {}
+    };
 
-  const assignStudents = async (mentorId, studentIds) => {
-    Toastify(assignServices.many({ mentorId, studentIds }), {
-      pending: "Assigning Students to Mentor",
+    const assignStudents = async (mentorId, studentIds) => {
+        Toastify(assignServices.many({ mentorId, studentIds }), {
+            pending: "Assigning Students to Mentor",
 
-      onSuccess: ({ data }) => {
-        getAssignedStudents(mentorId);
-        getUnassignedStudents();
-        return "Assigned Students to Mentor";
-      },
+            onSuccess: ({ data }) => {
+                getAssignedStudents(mentorId);
+                getUnassignedStudents();
+                return "Assigned Students to Mentor";
+            },
 
-      onError: (data) => {
-        return data?.response?.data?.message || "An Unexpected Error";
-      },
-    });
-  };
+            onError: (data) => {
+                return data?.response?.data?.message || "An Unexpected Error";
+            },
+        });
+    };
 
-  const unassignStudents = async (mentorId, studentIds) => {
-    Toastify(unassignServices.many({ studentIds }), {
-      pending: "UnAssigning Students",
+    const unassignStudents = async (mentorId, studentIds) => {
+        Toastify(unassignServices.many({ studentIds }), {
+            pending: "UnAssigning Students",
 
-      onSuccess: ({ data }) => {
-        getAssignedStudents(mentorId);
-        getUnassignedStudents();
-        return "UnAssigned the Students";
-      },
+            onSuccess: ({ data }) => {
+                getAssignedStudents(mentorId);
+                getUnassignedStudents();
+                return "UnAssigned the Students";
+            },
 
-      onError: (data) => {
-        return data?.response?.data?.message || "An Unexpected Error";
-      },
-    });
-  };
+            onError: (data) => {
+                return data?.response?.data?.message || "An Unexpected Error";
+            },
+        });
+    };
 
-  // useEffect(
-  //   () => {
-  //     fetchAll();
-  //   },
-  //   // eslint-disable-next-line
-  //   [batch_id]
-  // );
-  useEffect(
-    () => {
-      fetchAll();
-    },
-    // eslint-disable-next-line
-    []
-  );
+    // useEffect(
+    //   () => {
+    //     fetchAll();
+    //   },
+    //   // eslint-disable-next-line
+    //   [batch_id]
+    // );
 
-  return (
-    <MentorContext.Provider
-      value={{
-        get,
-        fetchAll,
-        mentors,
-        create,
-        update,
-        remove,
-        assignedStudents,
-        unassignedStudents,
-        getAssignedStudents,
-        getUnassignedStudents,
-        assignStudents,
-        unassignStudents,
-      }}
-    >
-      {children}
-    </MentorContext.Provider>
-  );
+    useEffect(
+        () => {
+            fetchAll();
+        },
+        // eslint-disable-next-line
+        []
+    );
+
+    return (
+        <MentorContext.Provider
+            value={{
+                get,
+                fetchAll,
+                mentors,
+                create,
+                update,
+                remove,
+                assignedStudents,
+                unassignedStudents,
+                getAssignedStudents,
+                getUnassignedStudents,
+                assignStudents,
+                unassignStudents,
+            }}>
+            {children}
+        </MentorContext.Provider>
+    );
 }
 
 export default MentorProvider;
